0fb4b7cd86298d80f75b1392c9b60c8d6ffacd4e
Owain Kenway <o.kenway@ucl.ac.uk>
2022-09-09 11:32:04 +0100

    Tidy

--- compare1.py
+++ compare1.py
@@ -1,25 +1,20 @@
 # This script compares the performance (time, error) for three algorithms
 # with a set number of interations.
 
-from piac import *
+import piac
 import sys
 import math
 
 def bench(lower, step, upper):
 	current = lower
 
-	# precompile
-	tt, tp, te = timefunc(integralpi, 1, 1)
-	tt, tp, te = timefunc(montecarlopi, 1, 1)
-	tt, tp, te = timefunc(gridpi, 1, 1)
-
 	print("Samples, Integral time, Integral error, Monte Carlo time, Monte Carlo error, Grid time, Grid error")
 	while (current < upper):
 		dim = math.floor(math.sqrt(current))
 		real = dim * dim
-		it, ip, ie = timefunc(integralpi, dim, dim)
-		mt, mp, me = timefunc(montecarlopi, dim, dim)
-		gt, gp, ge = timefunc(gridpi, dim, dim)
+		it, ip, ie = piac.timefunc(piac.integralpi, dim, dim)
+		mt, mp, me = piac.timefunc(piac.montecarlopi, dim, dim)
+		gt, gp, ge = piac.timefunc(piac.gridpi, dim, dim)
 		print(str(real) + ", " + str(it) + ", " + str(ie) + ", " + str(mt) + ", " + str(me) + ", " + str(gt) + ", " + str(ge) + ", " )
 
 		current += step
